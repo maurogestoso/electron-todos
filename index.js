42458247b31beb0bd7faa2117e799b2c4c839109
Mauro Gestoso <maurogestoso@gmail.com>
2018-01-13 15:34:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const electron = require("electron");
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+const menuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      { label: "New Todo" },
+      {
+        label: "Quit",
+        accelerator: process.platform === "darwin" ? "Command+Q" : "Control+Q",
+        click: () => {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+if (process.platform === "darwin") menuTemplate.unshift({});
