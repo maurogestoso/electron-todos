17efd3c508b34d6d3365d1718ebff600f2104100
Mauro Gestoso <maurogestoso@gmail.com>
2018-01-13 15:49:07 +0000

    Add addTodo window basic functionality

--- index.js
+++ index.js
@@ -2,25 +2,43 @@ const electron = require("electron");
 
 const { app, BrowserWindow, Menu } = electron;
 
-let mainWindow;
+let mainWindow, addWindow;
 
 app.on("ready", () => {
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/main.html`);
 
+  mainWindow.on("closed", () => app.quit());
+
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
 });
+
+const createAddWindow = () => {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: "Add New Todo"
+  });
+
+  addWindow.loadURL(`file://${__dirname}/add.html`);
+};
 
 const menuTemplate = [
   {
     label: "File",
     submenu: [
-      { label: "New Todo" },
+      {
+        label: "New Todo",
+        accelerator: process.platform === "darwin" ? "Command+N" : "Control+N",
+        click() {
+          createAddWindow();
+        }
+      },
       {
         label: "Quit",
         accelerator: process.platform === "darwin" ? "Command+Q" : "Control+Q",
-        click: () => {
+        click() {
           app.quit();
         }
       }
